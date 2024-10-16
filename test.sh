b91f341bd1c8db2451096482d8749271a328ea54
cauchy <841907997@qq.com>
2024-10-17 03:16:11 +0800

    modify test.sh to clean after each test

--- test.sh
+++ test.sh
@@ -5,8 +5,9 @@ branches=(util syscall pgtbl traps cow thread net lock fs mmap)
 for branch in "${branches[@]}"; do
     (
         git checkout "$branch" > /dev/null && \
-        make grade | grep -E "== |Score" \
-        || echo "Failed to grade on branch $branch"
+        make clean > /dev/null && \
+        make grade | grep -E "== |Score" && \
+        make clean > /dev/null
     )
 done
 git checkout main > /dev/null
